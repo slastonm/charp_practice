454a4f9040c1ce9b3b29d57cca67a3fb0494ca16
slastonm <slastonm@gmail.com>
2016-09-14 16:29:00 +0300

    4 home work

--- work1/work1/Program.cs
+++ work1/work1/Program.cs
@@ -61,10 +61,93 @@ namespace work1
             Console.WriteLine(areaMatch);
 
 
-
             //Console.WriteLine(a);
             //Console.WriteLine(b);
             //Console.WriteLine(c);
+
+
+            string translateWorld = Console.ReadLine();
+
+            switch (translateWorld)
+            {
+                case "bad": Console.WriteLine("bad2");
+                    break;
+                
+                case "good":
+                    Console.WriteLine("good2");
+                    break;
+                default:
+                    Console.WriteLine("i do not know");
+                    break;
+
+            }
+
+
+            string operand1, operand2, operationFunction;
+            Console.WriteLine("enter operand1");
+            operand1 = Console.ReadLine();
+            int operandOne = Int32.Parse(operand1);
+
+            Console.WriteLine("enter operand2");
+            operand2 = Console.ReadLine();
+            int operandTwo = Int32.Parse(operand2);
+
+            Console.WriteLine("enter operation");
+            operationFunction = Console.ReadLine();
+
+
+            switch (operationFunction)
+            {
+                case "+" : Console.WriteLine(operandOne + operandTwo);
+                    break;
+                case "-":
+                    Console.WriteLine(operandOne - operandTwo);
+                    break;
+                case "*":
+                    Console.WriteLine(operandOne * operandTwo);
+                    break;
+                case "/":
+                    if (operandTwo > 0)
+                    {
+                        Console.WriteLine(operandOne/operandTwo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+
+
+
+
+            Console.WriteLine("Введите число:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            if ((x >= 0) && (x <= 14))
+            {
+                Console.WriteLine("Число входит в промежуток [0 - 14]");
+            }
+            else if ((x >= 15) && (x <= 35))
+            {
+                Console.WriteLine("Число входит в промежуток [15 - 35]");
+            }
+            else if ((x >= 36) && (x <= 50))
+            {
+                Console.WriteLine("Число входит в промежуток [36 - 50]");
+            }
+            else if ((x >= 50) && (x <= 100))
+            {
+                Console.WriteLine("Число входит в промежуток [50 - 100]");
+            }
+            else
+            {
+                Console.WriteLine("Число не входит ни в один из существующих промежутков.");
+            }
 
             Console.ReadKey();
         }
